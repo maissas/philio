74a8e199b4a81169e029753b52a36c82b6a869dc
Maissa Saied <maissa@bilog.fr>
2022-10-02 02:32:39 +0100

    change db options

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   try{
     const dbURI = process.env.MONGODB_URI;
       //"mongodb+srv://maissa-admin:MKEsa12*@cluster0.oojg7l5.mongodb.net/philio_db_atlas?retryWrites=true&w=majority"
-    const mongodb = await mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    const mongodb = await mongoose.connect(dbURI, {useNewUrlParser: true, /*useUnifiedTopology: true,*/ useFindAndModify: false})
     console.log(`MongoDB connected: ${mongodb.connection.host}`)
   }catch(error){
     console.log(`MongoDB NOT connected !`)
